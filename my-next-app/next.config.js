1786cdda977bbb59557585942213b5030a829fce
Shubh1810 <your-github-email@example.com>
2024-12-07 10:36:21 -0800

    newwwbiee

--- my-next-app/next.config.js
+++ my-next-app/next.config.js
@@ -16,8 +16,14 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_API_URL },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { 
+            key: 'Access-Control-Allow-Origin', 
+            value: process.env.NEXT_PUBLIC_API_URL || '*' 
+          },
+          { 
+            key: 'Access-Control-Allow-Credentials', 
+            value: 'true' 
+          },
         ],
       },
     ];
